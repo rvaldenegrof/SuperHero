95177ca3e0d0e591452d915d25ed439d004ac380
rvaldenegrof <r.valdenegrof@gmail.com>
2021-06-16 23:35:41 -0400

    Se trabaja en llenar tarjeta del hÃeroe

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,6 @@ $("#buscar").click(function(){
     let resultado = validar(characterId);
     getHeroData(characterId);
 
-
     $("#txtId").val("");
 });
 
@@ -16,16 +15,35 @@ const getHeroData = (idHero) => {
 
         success: function (data) {
             console.log(data);
+            const imagenPerfil = data.image.url;
+            const nombreHero = data.name;
+            const publisherBy = data.biography.publisher;
+            const occupation = data.work.occupation;
+            const firstAparition = data.biography["first-appearance"];
+            const altura = data.appearance["height"];
+            console.log(altura);
+
+            $("#hero_thumb").attr("src", imagenPerfil);
+            
+            document.querySelector("#nombre_hero").innerHTML = `Nombre: ${nombreHero}`;
+            document.querySelector(".published_by").innerHTML = `Publicado por: ${publisherBy}`;
+            document.querySelector(".occ_hero").innerHTML = `Ocupación: ${occupation}`;
+            document.querySelector(".first_appearance").innerHTML = `Primera Aparición: ${firstAparition}`;
+            document.querySelector(".altura").innerHTML = `Altura: ${altura}`;
         }
     });
 }
+/* Zona de funciones */
 
+//VALIDAR 
 function validar(id_hero){
     let pasamosLaValidacion = true;
     let validacionNombre = /^[0-9]+$/gim
     if(validacionNombre.test(id_hero) == false){
         document.querySelector(".errorNombre").innerHTML = "Ingrese un ID válido";
         pasamosLaValidacion = false;
+    } else {
+        document.querySelector(".errorNombre").innerHTML = "";
     }
     return pasamosLaValidacion;
 }
