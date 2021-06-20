106f1075708a9a2262a7f1c5ba1a13b79e684f09
rvaldenegrof <r.valdenegrof@gmail.com>
2021-06-19 22:42:35 -0400

    Requerimiento finalizado

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,6 +14,7 @@ const getHeroData = (idHero) => {
         url: "https://www.superheroapi.com/api.php/"+tokenP+"/"+ idHero,
 
         success: function (data) {
+            $("#resultado").show();
             console.log(data);
             const imagenPerfil = data.image.url;
             const nombreHero = data.name;
@@ -21,15 +22,36 @@ const getHeroData = (idHero) => {
             const occupation = data.work.occupation;
             const firstAparition = data.biography["first-appearance"];
             const altura = data.appearance["height"];
-            console.log(altura);
+            const peso = data.appearance["weight"];
+            const alianza = data.biography.aliases;
+            const stats = data.powerstats;
+
+            const alturaB = replaceComa(altura);
+            const pesoB = replaceComa(peso);
+
+            
+            
+
 
             $("#hero_thumb").attr("src", imagenPerfil);
+            $("#hero_thumb").css("width", "200");
+
             
             document.querySelector("#nombre_hero").innerHTML = `Nombre: ${nombreHero}`;
             document.querySelector(".published_by").innerHTML = `Publicado por: ${publisherBy}`;
             document.querySelector(".occ_hero").innerHTML = `Ocupación: ${occupation}`;
             document.querySelector(".first_appearance").innerHTML = `Primera Aparición: ${firstAparition}`;
-            document.querySelector(".altura").innerHTML = `Altura: ${altura}`;
+            document.querySelector(".altura").innerHTML = `Altura: ${alturaB}`;
+            document.querySelector(".peso").innerHTML = `Peso: ${pesoB}`;
+            document.querySelector(".alianza").innerHTML = `Alianza: ${alianza}`;
+
+            const nombre = data["name"].toString().toUpperCase();
+
+            const dataPoints = []; 
+            for(keys in stats){
+                dataPoints.push({ y: parseInt(stats[keys])||0, label:keys})
+            }
+            renderChart(dataPoints, nombre); 
         }
     });
 }
@@ -46,4 +68,30 @@ function validar(id_hero){
         document.querySelector(".errorNombre").innerHTML = "";
     }
     return pasamosLaValidacion;
+}
+
+//RENDER CHART
+const renderChart = (dataPoints, nombre) => {
+    var chart = new CanvasJS.Chart("chartContainer", {
+        animationEnabled: true,
+        title: {
+            text: `Estadísticas de ${nombre}`
+        },
+        data: [
+            {
+                type: "pie",
+                startAngle: 25,
+                showInLegend: "true",
+                legendText: "{label}",
+                indexLabel: "{label} ({y})",
+                dataPoints: dataPoints
+            }
+        ]
+    });
+    chart.render();
+};
+
+//
+function replaceComa(a){
+    return a.join(' - ');
 }
